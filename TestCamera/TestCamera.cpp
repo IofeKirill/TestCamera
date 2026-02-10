950758343ccd4b3cdac42adb34e81cfe6720fcc9
IofeKirill <iofekd@adtspb.ru>
2026-02-10 20:57:58 +0300

    Набросал код для UDP (без теста!)

--- TestCamera/TestCamera.cpp
+++ TestCamera/TestCamera.cpp
@@ -63,6 +63,81 @@ int main()
 
 #include <opencv2/opencv.hpp> // подключение библиотеки opencv
 #include <vector>
+#include <winsock2.h> // основные сетевые типы и функции (сокеты)
+#include <ws2tcpip.h> // удобные функции для IP-адресов и портов
+#pragma comment(lib, "ws2_32.lib") // библиотека линковки WinSock (чтобы всё собралось).
+
+// Глобальные переменные сокета и адреса получателя
+SOCKET senderSocket = INVALID_SOCKET;  // сокет для отправки
+sockaddr_in receiverAddr{};            // структура с IP и портом получателя
+
+#pragma pack(push,1)
+struct Packet
+{
+    uint16_t x;  // координата X
+    uint16_t y;  // координата Y
+};
+#pragma pack(pop)
+
+bool initUDP() // инициализация UDP-соединения
+{
+    WSADATA wsa; // структура для запуска WinSock
+    // запускаем WinSock версии 2.2
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+        return false; // если ошибка — выходим
+
+    // создаём UDP-сокет (IPv4, датаграммы, протокол UDP)
+    senderSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    /*
+    * 1 параметр - тип адреса:
+    * AF_INET = IPv4.
+    * AF_INET6 → IPv6
+    * 
+    * 2 параметр - тип сокета:
+    * SOCK_DGRAM = датаграммы → это UDP.
+    * SOCK_STREAM → TCP (соединение, подтверждения, контроль доставки)
+    * 
+    * 3 параметр - Какой именно протокол использовать.
+    * IPPROTO_UDP → UDP
+    * IPPROTO_TCP → TCP
+    */
+
+    // проверяем, создан ли сокет
+    if (senderSocket == INVALID_SOCKET) 
+        return false;
+
+    receiverAddr.sin_family = AF_INET;      // используем IPv4
+    receiverAddr.sin_port = htons(239);     // порт получателя (перевод в сетевой формат)
+
+    // преобразуем строковый IP в бинарный формат
+    inet_pton(AF_INET, "192.168.4.1", &receiverAddr.sin_addr);
+
+    return true; // всё успешно
+}
+
+void shutdownUDP() // корректное завершение работы UDP
+{
+    // если сокет открыт — закрываем его
+    if (senderSocket != INVALID_SOCKET)
+        closesocket(senderSocket);
+
+    senderSocket = INVALID_SOCKET; // помечаем как закрытый
+
+    WSACleanup(); // освобождаем ресурсы WinSock
+}
+
+void sendData(const void* data, size_t size)
+{
+    sendto(
+        senderSocket,                              // сокет
+        reinterpret_cast<const char*>(data),       // указатель на данные
+        static_cast<int>(size),                    // размер данных
+        0,                                         // флаги
+        reinterpret_cast<sockaddr*>(&receiverAddr),// адрес получателя
+        sizeof(receiverAddr)                       // размер структуры адреса
+    );
+}
+
 
 struct MinMax
 {
@@ -79,6 +154,7 @@ int main()
     MinMax A = { 110, 120 };
     MinMax B = { 95, 120 };
     if (!video.isOpened()) return -1; // если камера не открылась — завершаем программу с кодом ошибки -1
+    if (!initUDP()) return -2; // инициализируем UDP
 
     while (true) // бесконечно
     {
@@ -128,6 +204,7 @@ int main()
         if (maxIndex != -1 && maxArea > 200)
         {
             cv::Point2f objectCenter;
+            Packet pack;
             float objectRadius = 0;
             cv::minEnclosingCircle(contour[maxIndex], objectCenter, objectRadius);
             cv::circle(frameBGR, objectCenter, (int)objectRadius, cv::Scalar(255, 0, 0), 2);
@@ -138,12 +215,23 @@ int main()
                 cv::FONT_HERSHEY_COMPLEX,
                 0.6,
                 cv::Scalar(0, 0, 255));
-        }
+            int xi = (int)std::lround(objectCenter.x);
+            int yi = (int)std::lround(objectCenter.y);
 
+            xi = std::clamp(xi, 0, 65535);
+            yi = std::clamp(yi, 0, 65535);
+
+            pack.x = htons((uint16_t)xi);
+            pack.y = htons((uint16_t)yi);
+
+            sendData(&pack, sizeof(pack));
+        }
+        
         imshow("OriginalVideo", frameBGR); // отображаем кадр в окне с именем OriginalVideo
         imshow("HSVVideo", frameHSV); // отображаем кадр в окне с именем HSVVideo
         imshow("LABVideo", frameLAB); // отображаем кадр в окне с именем LabVideo
         imshow("LabHSVVideo", frameLabHSV); // отображаем кадр в окне с именем LabHSVVideo
         if (cv::waitKey(1) == 27) break; // ожидание 1 мс и проверка нажатия клавиши ESC
     }
+    shutdownUDP(); // отключаем UDP
 }
